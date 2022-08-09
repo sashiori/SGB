314cd4386f7676cd7d16c56dc377fbbd93cf0898
Ryota Nakamura <45590100+RNMUDS@users.noreply.github.com>
2022-08-09 16:39:41 +0900

    Update auth_test.py

--- auth_test.py
+++ auth_test.py
@@ -6,8 +6,7 @@ gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
  
 # https://drive.google.com/drive/u/0/folders/#################################
-folder_id = '14RUXCD8xSkvUFMYu37YQopmda7vZSTbx'
-#folder_id = '1HpUBDPAVqvnOlFUxFwU-u3OjFdoxYO0_'
+folder_id = '####################################'
 file_name = '13cm_OK_2022-05-11_16-22-43_35.6311_139.7877.jpg'
  
 
